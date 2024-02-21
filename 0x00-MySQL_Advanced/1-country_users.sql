000a3c1e9ce38aaf2f0b011cacc94836a26b0f72
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-02-21 18:52:45 +0000

    .

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Creates table of users
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country VARCHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+);
+
